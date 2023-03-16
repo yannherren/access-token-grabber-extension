d84b8e9ee41ba4f1f3cc87c1f3bca30d6d684da4
yhe112114 <hycmedia@icloud.com>
2023-03-16 16:28:39 +0100

    create working version

--- src/background.ts
+++ src/background.ts
@@ -1,17 +1,23 @@
-
 let authorizationToken: string | undefined = '';
 
 chrome.webRequest.onSendHeaders.addListener(
     async (req) => {
-      if (req.requestHeaders) {
-        authorizationToken = req.requestHeaders.find(it => it.name === 'authorization')?.value;
-        if (!authorizationToken) {
-            return;
+        const onData = await chrome.storage.local.get('on')
+        const headerNameData = await chrome.storage.local.get('headerName')
+        if (onData['on']) {
+            if (req.requestHeaders) {
+                const headerName =  (headerNameData['headerName'] ? headerNameData['headerName'] : 'authorization');
+                authorizationToken = req.requestHeaders.find(it => it.name === headerName)?.value;
+                if (!authorizationToken) {
+                    return;
+                }
+                await chrome.action.setBadgeText({text: '1'});
+                await chrome.storage.local.set({latestAuthToken: authorizationToken})
+            }
+        } else {
+            await chrome.storage.local.set({latestAuthToken: ''})
         }
-        await chrome.action.setBadgeText({ text: '1'});
-        await chrome.storage.local.set({ latestAuthToken: authorizationToken })
-      }
     },
-    {urls: [ "<all_urls>" ]},
+    {urls: ["<all_urls>"]},
     ['requestHeaders']
 )
--- src/popup.tsx
+++ src/popup.tsx
@@ -4,14 +4,44 @@ import styles from "./popup.module.css"
 import {clipboard} from "@extend-chrome/clipboard";
 
 const Popup = () => {
+
     const [tokenCopied, setTokenCopied] = useState(false);
+    const [headerName, setHeaderName] = useState('authorization');
+    const [listening, setListening] = useState<null | boolean>(null);
 
     useEffect(() => {
+        setTimeout(() => {
+            chrome.storage.local.set({headerName})
+        }, 200)
+    }, [headerName])
+
+    useEffect(() => {
+        if (listening === null) {
+            return
+        }
+        if (listening) {
+            chrome.action.setIcon({path: '/on.png'})
+            chrome.storage.local.set({on: true})
+        } else {
+            chrome.action.setIcon({path: '/off.png'});
+            chrome.action.setBadgeText({text: ''});
+            chrome.storage.local.set({latestAuthToken: ''})
+            chrome.storage.local.set({on: false})
+        }
+    }, [listening])
+
+    useEffect(() => {
+        chrome.storage.local.get('headerName').then(val => {
+            setHeaderName(val['headerName'])
+        })
+        chrome.storage.local.get('on').then(val => {
+            setListening(val['on'])
+        })
         chrome.storage.local.get('latestAuthToken').then(async (entries) => {
             if (entries.latestAuthToken) {
                 const token = entries.latestAuthToken
                 await clipboard.writeText(token);
-                await chrome.storage.local.set({ latestAuthToken: undefined })
+                await chrome.storage.local.set({latestAuthToken: undefined})
                 setTokenCopied(true)
             } else {
                 setTokenCopied(false);
@@ -24,15 +54,29 @@ const Popup = () => {
         <div className={styles.container}>
             <h1 className={styles.title}>Access Token Grabber</h1>
             <div>
+                <span>Listening for tokens:</span>
+                <br/><br/>
+                {listening ?
+                    <button className={styles.on} onClick={() => setListening(false)}>ON</button>
+                    : <button className={styles.off} onClick={() => setListening(true)}>OFF</button>
+                }
+            </div>
+            <div className={styles.section}>
                 <span>Look for header named:</span>
-                <br /><br />
-                <input value="authorization"/>
+                <br/><br/>
+                <input className={styles.input} value={headerName} onChange={(e) => setHeaderName(e.target.value)}/>
             </div>
             <div className={styles.message}>
                 {tokenCopied ?
-                    <span>✅ Copied the latest token to your clipboard, use it wisely! 😃</span>
+                    <div className={styles.success}>
+                        <span className={styles.thumb}>👍</span>
+                        <br/>
+                        <span>Copied the latest token to your clipboard, use it wisely!</span>
+                    </div>
                     :
-                    <span>Seems like there is no token available yet! Try to make a web request to receive a token.</span>
+                    listening ?
+                        <span>Seems like there is no token available yet! Try to make a web request to receive a token.</span>
+                        : ''
                 }
             </div>
         </div>
